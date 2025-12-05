0af2168738fad08d6ce3de89dad8d534784cfafd
aspasiax <asxafi@gmail.com>
2025-12-05 10:41:01 +0200

    feat: add MenuApp demo with console menu

---
+++ src/gr/aueb/cf/cf9/part1pop/ch1/MenuApp.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.cf9.part1pop.ch1;
+
+/**
+ * A simple application that prints a menu of options
+ * to the standard output (console).
+ */
+
+public class MenuApp {
+
+    public static void main(String[] args) {
+        System.out.println("Select one of the following options:");
+        System.out.println("1. Insert");
+        System.out.println("2. Delete");
+        System.out.println("3. Search");
+        System.out.println("4. Update");
+        System.out.println("5. Exit");
+
+        System.out.println("Please enter your choice number: ");
+    }
+}
