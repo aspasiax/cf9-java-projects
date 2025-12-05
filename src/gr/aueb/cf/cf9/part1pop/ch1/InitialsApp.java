6ac0299d98f20fd721e6c0e173e1d1efef4a50a3
aspasiax <asxafi@gmail.com>
2025-12-05 10:42:48 +0200

    feat: add InitialsApp demo with ASCII-style initials

---
+++ src/gr/aueb/cf/cf9/part1pop/ch1/InitialsApp.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.cf9.part1pop.ch1;
+
+/**
+ * Prints the initials "X A" in block-style letters
+ * using asterisks (*) on the standard output.
+ */
+
+public class InitialsApp {
+
+    public static void main(String[] args) {
+        System.out.println("*       *        *       ");
+        System.out.println(" *     *        * *      ");
+        System.out.println("  *   *        *   *     ");
+        System.out.println("    *         *******    ");
+        System.out.println("  *   *      *       *   ");
+        System.out.println(" *     *    *         *  ");
+        System.out.println("*       *  *           * ");
+    }
+}
